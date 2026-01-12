360729007a4260f1517d0efbe404d8f9469e3ee7
Lobo <octavopineapple@gmail.com>
2026-01-12 02:31:32 -0600

    Modify older practices, headers files and first part of SPI Comm LED

--- Src/008spi_cmd_handling.c
+++ Src/008spi_cmd_handling.c
@@ -16,6 +16,34 @@
 #include <string.h>
 #include "stm32f411xx.h"
 
+//command codes
+#define COMMAND_LED_CTRL         	0x50
+#define COMMAND_SENSOR_READ      	0x51
+#define COMMAND_LED_READ         	0x52
+#define COMMAND_PRINT           	0x53
+#define COMMAND_ID_READ         	0x54
+
+#define LED_ON     1
+#define LED_OFF    0
+
+//arduino analog pins
+#define ANALOG_PIN0   0
+#define ANALOG_PIN1   1
+#define ANALOG_PIN2   2
+#define ANALOG_PIN3   3
+#define ANALOG_PIN4   4
+
+//arduino led
+#define LED_PIN	8
+
+uint8_t SPI_VerifyResponse(uint8_t ackbyte){
+	if(ackbyte == 0xF5){
+		//ack
+		return 1;
+	}
+	return 0;
+}
+
 void delay(void)
 {
 	for(uint32_t i = 0 ; i < 500000/2 ; i ++);
@@ -40,8 +68,8 @@ void SPI3_GPIOInits(void){
 	GPIO_Init(&SPIPins);
 
 	//MISO
-	//SPIPins.GPIO_PinConfig.GPIO_PinNum = GPIO_PIN4;
-	//GPIO_Init(&SPIPins);
+	SPIPins.GPIO_PinConfig.GPIO_PinNum = GPIO_PIN4;
+	GPIO_Init(&SPIPins);
 
 	//NSS
 	SPIPins.pGPIOx = GPIOA;
@@ -57,7 +85,7 @@ void SPI3_Inits(void){
 	SPI3Handler.pSPIx = SPI3;
 	SPI3Handler.SPIConfig.SPI_DeviceMode = SPI_DEVICE_MODE_MASTER;
 	SPI3Handler.SPIConfig.SPI_BusConfig = SPI_BUS_CONFIG_FD;
-	SPI3Handler.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV8; // Generates SCLK of 2MHz
+	SPI3Handler.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV16; // Generates SCLK of 500kHz
 	SPI3Handler.SPIConfig.SPI_DFF = SPI_DFF_8BITS;
 	SPI3Handler.SPIConfig.SPI_CPOL = SPI_CPOL_LOW;
 	SPI3Handler.SPIConfig.SPI_CPHA = SPI_CPHA_LOW;
@@ -81,7 +109,7 @@ void GPIOBtn_Init(void)
 
 int main(void){
 
-	char user_data[] = "Mi nombre es Luis Octavio Pina Hernandez, quiero compartir este logro, desarrolle mi primer driver tanto para GPIO como para SPI";
+	//uint8_t dummy_write = 0xff;
 
 	GPIOBtn_Init();
 
@@ -99,22 +127,42 @@ int main(void){
 
 	while(1){
 		//Wait till the button is pressed
-		while(! GPIO_ReadFromInputPin(GPIOA, GPIO_PIN0));
+		while(GPIO_ReadFromInputPin(GPIOC, GPIO_PIN13) == 1);
 
 		delay();
+
+		// Espera a que se suelte (para no repetir)
+		while(GPIO_ReadFromInputPin(GPIOC, GPIO_PIN13) == 0);
 
 		//Enable the SPI3 Peripheral
 		SPI_PeripheralControl(SPI3, ENABLE);
 
-		//First send data length
-		uint8_t datalen =  strlen(user_data);
-		SPI_SendData(SPI3, &datalen, 1);
+//		1. CMD_LED_CTRL <pin no(1)> <value(1)>
+		uint8_t cmdCode = COMMAND_LED_CTRL;
+		uint8_t ackByte, dummy_read;
+		uint8_t args[2];
+		SPI_SendData(SPI3, &cmdCode, 1);
+		//do dummy read to clear off the RXNE
+		SPI_ReceiveData(SPI3, &ackByte, 1);
 
-		//SendData
-		SPI_SendData(SPI3, (uint8_t*)user_data, strlen(user_data));
+////		send some dummy bits (1byte) to fetch the respone from the slave.
+//		SPI_SendData(SPI3, &dummy_write, 1);
+////		read the ack byte received
+//		SPI_ReceiveData(SPI3, &ackByte, 1);
+
+		if(SPI_VerifyResponse(ackByte)){
+			//send args
+			args[0] = LED_PIN;
+			args[1] = LED_ON;
+			SPI_SendData(SPI3, args, 2);
+		    // limpiar RX por los 2 bytes recibidos mientras transmitías
+		    SPI_ReceiveData(SPI3, &dummy_read, 1);
+		    SPI_ReceiveData(SPI3, &dummy_read, 1);
+		}
 
 		//SPI its not BSY
-		while( SPI_GetFlagStatus(SPI3, SPI_BUSY_FLAG));
+		while(SPI_GetFlagStatus(SPI3, SPI_TXE_FLAG) == FLAG_RESET);
+		while(SPI_GetFlagStatus(SPI3, SPI_BUSY_FLAG) == FLAG_SET);
 
 		//Disable the SPI3 Peripheral
 		SPI_PeripheralControl(SPI3, DISABLE);
